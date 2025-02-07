87896bce2982e7e41966ffe355762ccfc54d9c92
diegoszel <diegoh23@gmail.com>
2025-02-07 11:46:25 +0200

    weather update

--- JavaScript Projects/Weather/scripts/CurrentWidget.js
+++ JavaScript Projects/Weather/scripts/CurrentWidget.js
@@ -1,43 +1,61 @@
 export class CurrentWidget {
-    #htmlElement = document.createElement("div");
-    #icon = document.createElement("img");
-    #timeElement = document.createElement("div");
-    #dateElement = document.createElement("div");
+  #htmlElement = document.createElement("div");
+  #icon = document.createElement("img");
+  #timeElement = document.createElement("div");
+  #dateElement = document.createElement("div");
+  #cityElement = document.createElement("p");
 
-    constructor() {
-        this.#htmlElement.className = "current-widget";
-        const textWrapper = document.createElement("div");
-        textWrapper.className = "text-wrapper";
-        const timeWrappper = document.createElement("div");
-        timeWrappper.className = "time-wrapper";
-        this.#timeElement.className = "time";
-        this.#dateElement.className = "date";
-        textWrapper.appendChild(timeWrappper);
-        timeWrappper.appendChild(this.#timeElement);
-        textWrapper.appendChild(this.#dateElement);
-        this.#htmlElement.appendChild(textWrapper);
-        this.#htmlElement.appendChild(this.#icon);
-    }
+  constructor() {
+    this.#htmlElement.className = "current-widget";
+    this.#cityElement.className = "city-name";
+    this.#htmlElement.appendChild(this.#cityElement);
+    const textWrapper = document.createElement("div");
+    textWrapper.className = "text-wrapper";
+    const timeWrappper = document.createElement("div");
+    timeWrappper.className = "time-wrapper";
+    this.#timeElement.className = "time";
+    this.#dateElement.className = "date";
+    textWrapper.appendChild(timeWrappper);
+    timeWrappper.appendChild(this.#timeElement);
+    textWrapper.appendChild(this.#dateElement);
+    this.#htmlElement.appendChild(this.#icon);
+    this.#htmlElement.appendChild(textWrapper);
+  }
 
-    async update(data) {
-        const date = new Date();
-        const iconResponse = await fetch(`https://openweathermap.org/img/wn/${data[0].weather[0].icon}@2x.png`);
-        this.#icon.src = iconResponse.url;
-        this.#icon.alt = data[0].weather[0].description;
-        let timezone = data[0].timezone;
-        const positive = timezone >= 0 ? true : false;
-        timezone = Math.abs(timezone);
-        let timezoneHours = timezone / 3600; //seconds in an hour
-        if (timezoneHours < 10) timezoneHours = "0" + timezoneHours;
-        let timezoneMinutes = timezone % 3600 / 60; //seconds in a minute of the leftover time
-        if (timezoneMinutes < 10) timezoneMinutes = "0" + timezoneMinutes;
-        const timezoneAsHHMM = (positive ? "+" : "-") + String(timezoneHours) + String(timezoneMinutes);
-        const options = { timeZone: timezoneAsHHMM };
-        this.#timeElement.textContent = date.toLocaleTimeString([], { timeZone: timezoneAsHHMM, hour: "2-digit", minute: "2-digit", hour12: false });
-        this.#dateElement.textContent = date.toLocaleDateString([], { timeZone: timezoneAsHHMM, weekday: "long", year: "2-digit", month: "2-digit", day: "2-digit" });
-    }
+  async update(data) {
+    const date = new Date();
+    const iconResponse = await fetch(
+      `https://openweathermap.org/img/wn/${data[0].weather[0].icon}@2x.png`
+    );
+    this.#cityElement.textContent = data[0].name;
+    this.#icon.src = iconResponse.url;
+    this.#icon.alt = data[0].weather[0].description;
+    let timezone = data[0].timezone;
+    const positive = timezone >= 0 ? true : false;
+    timezone = Math.abs(timezone);
+    let timezoneHours = timezone / 3600; //seconds in an hour
+    if (timezoneHours < 10) timezoneHours = "0" + timezoneHours;
+    let timezoneMinutes = (timezone % 3600) / 60; //seconds in a minute of the leftover time
+    if (timezoneMinutes < 10) timezoneMinutes = "0" + timezoneMinutes;
+    const timezoneAsHHMM =
+      (positive ? "+" : "-") + String(timezoneHours) + String(timezoneMinutes);
+    const options = { timeZone: timezoneAsHHMM };
+    this.#timeElement.textContent = date.toLocaleTimeString([], {
+      timeZone: timezoneAsHHMM,
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+    this.#dateElement.textContent = date.toLocaleDateString([], {
+      timeZone: timezoneAsHHMM,
+      weekday: "long",
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit",
+    });
+  }
 
-    get htmlElement() {
-        return this.#htmlElement;
-    }
+  get htmlElement() {
+    return this.#htmlElement;
+  }
 }
